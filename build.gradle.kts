519c16911fec51bb9f3998d68f9b3b16adfb013a
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-03 11:50:38 +0000

    Run from command line

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.7.22"
+    application
 }
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set(project.property("day")?.let { "day$it.CodeKt" })
 }
 
 tasks {
