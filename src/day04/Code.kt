5eb6b50ebe6b31b8e0e0f6a70ca5af9ff3383820
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-04 11:41:32 +0000

    improve a bit

--- src/day04/Code.kt
+++ src/day04/Code.kt
@@ -2,20 +2,20 @@ package day04
 
 import readInput
 operator fun IntRange.contains(other: IntRange) = first in other && last in other
-fun IntRange.overlaps(other: IntRange) = first in other || last in other
-fun String.toRange(delimiter: Char = '-') = split(delimiter).map { it.toInt() }.let { it[0]..it[1] }
+fun IntRange.overlaps(other: IntRange) = first in other || last in other || other.first in this || other.last in this
+fun String.toIntRange(delimiter: Char = '-') = split(delimiter).map { it.toInt() }.let { it[0]..it[1] }
 
 fun main() {
 
     fun parse(input: List<String>) : List<Pair<IntRange,IntRange>> = input.map { it.split(',') }.map {
-        (first,second) -> first.toRange() to second.toRange()
+        (first,second) -> first.toIntRange() to second.toIntRange()
     }
     fun part1(input: List<String>) = parse(input).count { (first,second) ->
         first in second || second in first
     }
 
     fun part2(input: List<String>) =  parse(input).count { (first,second) ->
-        first.overlaps(second) || second.overlaps(first)
+        first.overlaps(second)
     }
 
     val input = readInput(::main.javaClass.packageName)
