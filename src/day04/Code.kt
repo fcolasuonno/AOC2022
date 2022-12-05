d05ae0ce81f81d43af453633a8455f68045394c0
fcolasuonno <fcolasuonno@users.noreply.github.com>
2022-12-05 00:02:31 +0000

    regexing

--- src/day04/Code.kt
+++ src/day04/Code.kt
@@ -2,24 +2,32 @@ package day04
 
 import readInput
 
+
 operator fun IntRange.contains(other: IntRange) = first in other && last in other
 fun IntRange.overlaps(other: IntRange) = first <= other.last && last >= other.first 
 fun String.toIntRange(delimiter: Char = '-') = split(delimiter).map { it.toInt() }.let { it[0]..it[1] }
 
-fun main() {
+typealias Day04Type = Pair<IntRange,IntRange>
 
-    fun parse(input: List<String>) : List<Pair<IntRange,IntRange>> = input.map { it.split(',') }.map {
-        (first,second) -> first.toIntRange() to second.toIntRange()
+fun main() {
+    val format = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+    fun parse(input: List<String>) = input.map { line -> 
+        format.matchEntire(line)!!.destructured
+            .toList().map(String::toInt)
+            .let { (x1, x2, y1, y2) ->
+                x1..x2 to y1..y2 
+            }
     }
-    fun part1(input: List<String>) = parse(input).count { (first,second) ->
+
+    fun part1(input: List<Day04Type>) = input.count { (first,second) ->
         first in second || second in first
     }
 
-    fun part2(input: List<String>) =  parse(input).count { (first,second) ->
+    fun part2(input: List<Day04Type>) = input.count { (first,second) ->
         first.overlaps(second)
     }
 
-    val input = readInput(::main.javaClass.packageName)
+    val input = parse(readInput(::main.javaClass.packageName))
     println(part1(input))
     println(part2(input))
 }
