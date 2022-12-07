5833b6002846b889cc0c0f7bc1a69acbb2bb8533
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-07 11:03:44 +0000

    day 7

--- src/day07/Code.kt
+++ src/day07/Code.kt
@@ -1,14 +1,61 @@
 package day07
 
-import day06.main
 import readInput
 
+sealed class Day06Node {
+    abstract val size: Int
+}
+
+private class File(override val size: Int) : Day06Node()
+private class Dir(
+    val fileContents: MutableMap<String, File> = mutableMapOf(),
+    val dirContents: MutableMap<String, Dir> = mutableMapOf(),
+    val parent: Dir? = null
+) : Day06Node(), Iterable<Dir> {
+    override val size: Int by lazy {
+        fileContents.values.sumOf { it.size } +
+                dirContents.values.sumOf { it.size }
+    }
+
+    override fun iterator() = iterator {
+        yield(this@Dir)
+        dirContents.values.forEach { yieldAll(it) }
+    }
+}
+
 fun main() {
-    fun part1(input: List<String>) = input.size
+    fun parse(input: List<String>) = Dir().apply { ->
+        input.filter { it != "$ ls" } //ls is useLS
+            .drop(1) //already created root node
+            .map { it.split(" ") }
+            .fold(this) { cwd, command ->
+                if (command[0] == "$" && command[1] == "cd") {
+                    val newDir = command[2]
+                    requireNotNull(if (newDir == "..") cwd.parent else cwd.dirContents[newDir]) //new cwd
+                } else {
+                    cwd.apply {
+                        val name = command[1]
+                        if (command[0] == "dir") {
+                            dirContents[name] = Dir(parent = cwd)
+                        } else {
+                            fileContents[name] = File(command[0].toInt())
+                        }
+                    }
+                }
+            }
+    }
 
-    fun part2(input: List<String>) = input.size
+    fun part1(input: Dir) = input.map { it.size }.filter { it < 100000 }.sumOf { it }
 
-    val input = readInput(::main.javaClass.packageName)
+    fun part2(input: Dir): Int {
+        val total = 70000000
+        val free = total - input.size
+        val required = 30000000 - free
+        return input.map { it.size }.filter { it >= required }.minOf { it }
+    }
+
+    val input = parse(readInput(::main.javaClass.packageName))
     println("Part1=\n" + part1(input))
     println("Part2=\n" + part2(input))
 }
+
---
+++ src/day08/Code.kt
@@ -0,0 +1,14 @@
+package day08
+
+import day06.main
+import readInput
+
+fun main() {
+    fun part1(input: List<String>) = input.size
+
+    fun part2(input: List<String>) = input.size
+
+    val input = readInput(::main.javaClass.packageName)
+    println("Part1=\n" + part1(input))
+    println("Part2=\n" + part2(input))
+}
