90933416a6c25f80db799f39cdfaa4f04b7e07f9
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-08 14:23:17 +0000

    arrays are maps, no?

--- src/day08/Code.kt
+++ src/day08/Code.kt
@@ -4,55 +4,47 @@ import Coord
 import day06.main
 import readInput
 
-data class Forest(val trees: Map<Coord, Int>, val xMax: Int, val yMax: Int) : Iterable<Coord> by trees.keys {
-    fun isVisible(coord: Coord): Boolean {
-        val (x, y) = coord
-        val tree = trees[coord]!!
-        return (0 until x).all {
-            trees[Coord(it, y)]!! < tree
-        } || (0 until y).all {
-            trees[Coord(x, it)]!! < tree
-        } || ((x + 1) until xMax).all {
-            trees[Coord(it, y)]!! < tree
-        } || ((y + 1) until yMax).all {
-            trees[Coord(x, it)]!! < tree
-        }
+typealias IntGrid = Array<Array<Int>>
+
+class Forest(private val trees: IntGrid) : Iterable<Coord> {
+    val maxX = trees[0].size
+    val maxY = trees.size
+    fun isVisible(x: Int, y: Int) = (0 until x).all { trees[it][y] < trees[x][y] }
+            || (0 until y).all { trees[x][it] < trees[x][y] }
+            || ((x + 1) until maxX).all { trees[it][y] < trees[x][y] }
+            || ((y + 1) until maxY).all { trees[x][it] < trees[x][y] }
+
+    fun scenicScore(x: Int, y: Int): Int {
+        val left = ((x - 1) downTo 1).takeWhile { trees[it][y] < trees[x][y] }.size
+        val top = ((y - 1) downTo 1).takeWhile { trees[x][it] < trees[x][y] }.size
+        val right = ((x + 1) until (maxX - 1)).takeWhile { trees[it][y] < trees[x][y] }.size
+        val down = ((y + 1) until (maxY - 1)).takeWhile { trees[x][it] < trees[x][y] }.size
+        return (left + 1) * (top + 1) * (right + 1) * (down + 1)
     }
 
-    fun scenicScore(coord: Coord): Int {
-        val (x, y) = coord
-        val tree = trees[coord]!!
-        val left = ((x - 1) downTo 1).takeWhile {
-            trees[Coord(it, y)]!! < tree
-        }.size
-        val top = ((y - 1) downTo 1).takeWhile {
-            trees[Coord(x, it)]!! < tree
-        }.size
-        val right = ((x + 1) until (xMax - 1)).takeWhile {
-            trees[Coord(it, y)]!! < tree
-        }.size
-        val down = ((y + 1) until (yMax - 1)).takeWhile {
-            trees[Coord(x, it)]!! < tree
-        }.size
-        return (left + 1) * (top + 1) * (right + 1) * (down + 1)
+    override fun iterator() = iterator {
+        (1 until (maxX - 1)).forEach { x ->
+            (1 until (maxY - 1)).forEach { y ->
+                yield(Coord(x, y))
+            }
+        }
     }
 }
 
 fun main() {
-    fun parse(input: List<String>) = Forest(
-        input.flatMapIndexed { y, s -> s.mapIndexed { x, c -> Coord(x, y) to c.digitToInt() } }.toMap(),
-        input.first().length,
-        input.size
-    )
-
-    fun part1(input: Forest) = input.count {
-        input.isVisible(it)
-    }
-
-    fun part2(input: Forest) = input.filter { (x, y) -> x != 0 && y != 0 && x != input.xMax - 1 && y != input.yMax - 1 }
-        .maxOf {
-            input.scenicScore(it)
+    fun parse(input: List<String>) = Forest(Array(input.first().length) { x ->
+        Array(input.size) { y ->
+            input[x][y].digitToInt()
         }
+    })
+
+    fun part1(input: Forest) = input.count { (x, y) ->
+        input.isVisible(x, y)
+    } + (input.maxX - 1 + input.maxY - 1) * 2 //edges
+
+    fun part2(input: Forest) = input.maxOf { (x, y) ->
+        input.scenicScore(x, y)
+    }
 
     val input = parse(readInput(::main.javaClass.packageName))
     println("Part1=\n" + part1(input))
