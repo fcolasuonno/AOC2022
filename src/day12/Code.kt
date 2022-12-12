cb7bdc07b51b495617f535e9764cef5b81905dfd
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-12 16:56:01 +0000

    A bit better

--- src/day12/Code.kt
+++ src/day12/Code.kt
@@ -20,29 +20,23 @@ fun main() {
         return Triple(start, end, heightmap)
     }
 
-    fun part1(start: Coord, end: Coord, heightmap: Map<Coord, Char>) =
-        generateSequence(setOf(start to 'a') to mutableSetOf<Coord>()) { (frontier, seen) ->
-            frontier.flatMap { (coord, elevation) ->
-                coord.closeNeighbours.filter { it !in seen }.mapNotNull { newCoord ->
-                    heightmap[newCoord]?.takeIf { it - elevation <= 1 }?.let { newCoord to it }
-                }
-            }.toSet().takeIf { end !in seen }?.let { newFrontier ->
-                seen += newFrontier.map { it.first }
-                newFrontier to seen
-            }
-        }.count()
+    fun part1(start: Coord, end: Coord, heightmap: Map<Coord, Char>) = mutableSetOf<Coord>().let { seen ->
+        fun Coord.isAccessible(from: Coord) = heightmap[this]?.takeIf { it - heightmap[from]!! <= 1 } != null
+        generateSequence(setOf(start)) { frontier ->
+            frontier.flatMap { coord ->
+                coord.closeNeighbours.filter { newCoord -> newCoord !in seen && newCoord.isAccessible(coord) }
+            }.toSet().takeIf { end !in seen }?.also { seen += it }
+        }
+    }.count()
 
-    fun part2(end: Coord, heightmap: Map<Coord, Char>) =
-        generateSequence(setOf(end to 'z') to mutableSetOf<Coord>()) { (frontier, seen) ->
-            frontier.flatMap { (coord, elevation) ->
-                coord.closeNeighbours.filter { it !in seen }.mapNotNull { newCoord ->
-                    heightmap[newCoord]?.takeIf { elevation - it <= 1 }?.let { newCoord to it }
-                }
-            }.toSet().takeIf { it.none { (_, elevation) -> elevation == 'a' } }?.let { newFrontier ->
-                seen += newFrontier.map { it.first }
-                newFrontier to seen
-            }
-        }.count()
+    fun part2(end: Coord, heightmap: Map<Coord, Char>) = mutableSetOf<Coord>().let { seen ->
+        fun Coord.isAccessible(from: Coord) = heightmap[this]?.takeIf { heightmap[from]!! - it <= 1 } != null
+        generateSequence(setOf(end)) { frontier ->
+            frontier.flatMap { coord ->
+                coord.closeNeighbours.filter { newCoord -> newCoord !in seen && newCoord.isAccessible(coord) }
+            }.toSet().takeIf { it.none { heightmap[it] == 'a' } }?.also { seen += it }
+        }
+    }.count()
 
     val (start, end, heightmap) = parse(readInput(::main.javaClass.packageName))
     println("Part1=\n" + part1(start, end, heightmap))
