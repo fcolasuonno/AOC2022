c80adb870641523d16a2c059afdeb1fc042858c6
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-12 07:16:42 +0000

    WIP

--- src/day12/Code.kt
+++ src/day12/Code.kt
@@ -1,10 +1,21 @@
 package day12
 
+import Coord
 import day06.main
 import readInput
 
 fun main() {
-    fun part1(input: List<String>) = input.size
+    fun part1(input: List<String>): Int {
+        val map = input.flatMapIndexed { y, s -> s.mapIndexed { x, c -> Coord(x, y) to c } }.toMap()
+        val start = map.entries.first { it.value == 'S' }.key
+        val end = map.entries.first { it.value == 'E' }.key
+        val map1 = map.filterKeys { it != start && it != end }.mapValues { assert(it.value in 'a'..'z')
+            it.value-'a' }
+        System.err.println(map1)
+        generateSequence(listOf( start to 0)) {it
+        }
+        return map1.size
+    }
 
     fun part2(input: List<String>) = input.size
 
