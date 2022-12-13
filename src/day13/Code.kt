abbc3387043b136dadb391b5510b69acba39e342
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-13 12:41:28 +0000

    Same

--- src/day13/Code.kt
+++ src/day13/Code.kt
@@ -1,32 +1,36 @@
 package day13
 
 import day06.main
-import grouped
 import readInput
 
-sealed class Packet : Comparable<Packet>
-
-data class IntPacket(val value: Int) : Packet() {
-    override fun compareTo(other: Packet) = if (other is IntPacket) {
-        value - other.value
-    } else {
-        ListPacket(listOf(this)).compareTo(other)
-    }
-}
-
-data class ListPacket(val contents: List<Packet>) : Packet() {
-    override fun compareTo(other: Packet): Int = if (other is ListPacket) {
-        contents.zip(other.contents, Packet::compareTo).firstOrNull { it != 0 } ?: (contents.size - other.contents.size)
-    } else {
-        compareTo(ListPacket(listOf(other)))
-    }
-}
-
 fun main() {
-    fun parseInt(chars: MutableList<Char>) =
-        chars.takeWhile(Char::isDigit).fold(0) { acc, c -> acc * 10 + (c - '0') }.also {
-            while (chars.first().isDigit()) chars.removeFirst()
+    abstract class Packet : Comparable<Packet>
+    data class ListPacket(val contents: List<Packet>) : Packet() {
+        override fun compareTo(other: Packet): Int =
+            if (other is ListPacket) contents
+                .zip(other.contents, Packet::compareTo)
+                .firstOrNull { it != 0 }
+                ?: (contents.size - other.contents.size)
+            else compareTo(ListPacket(listOf(other)))
+    }
+
+    data class IntPacket(val value: Int) : Packet() {
+        override fun compareTo(other: Packet) =
+            if (other is IntPacket) value - other.value
+            else ListPacket(listOf(this)).compareTo(other)
+    }
+
+    fun <E> MutableCollection<E>.removeWhile(predicate: (E) -> Boolean) = iterator().apply {
+        while (hasNext() && predicate(next())) {
+            remove()
         }
+    }
+
+    fun List<Char>.toInt() = fold(0) { acc, c -> acc * 10 + (c - '0') }
+
+    fun parseInt(chars: MutableList<Char>) = chars.takeWhile(Char::isDigit).toInt().also {
+        chars.removeWhile(Char::isDigit)
+    }
 
     fun parseList(chars: MutableList<Char>): ListPacket = ListPacket(buildList {
         chars.removeFirst()
@@ -41,18 +45,20 @@ fun main() {
     })
 
     fun parse(input: List<String>) =
-        input.map { it.takeIf(String::isNotEmpty)?.let { chars -> parseList(chars.toMutableList()) } }
+        input.mapNotNull { it.takeIf(String::isNotEmpty)?.let { chars -> parseList(chars.toMutableList()) } }
 
-    fun part1(input: List<ListPacket?>) =
-        input.grouped().map { (left, right) -> left < right }.foldIndexed(0) { index, acc, inOrder ->
+    fun part1(input: List<ListPacket>) = input.chunked(2)
+        .map { (left, right) -> left < right }
+        .foldIndexed(0) { index, acc, inOrder ->
             if (inOrder) acc + index + 1 else acc
         }
 
-    fun part2(input: List<ListPacket?>) = setOf(
+    fun part2(input: List<ListPacket>) = setOf(
         parseList("[[2]]".toMutableList()),
         parseList("[[6]]".toMutableList())
     ).let { dividers ->
-        input.asSequence().filterNotNull().plus(dividers).sortedWith(Packet::compareTo).map { it in dividers }
+        input.plus(dividers).sortedWith(Packet::compareTo)
+            .map { it in dividers }
             .foldIndexed(1) { index, acc, isDivider ->
                 if (isDivider) acc * (index + 1) else acc
             }
