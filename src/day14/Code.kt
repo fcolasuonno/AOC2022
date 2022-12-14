15a6887abc001e8ad09dbd226d16b59347669741
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-14 12:45:23 +0000

    Better and faster

--- src/day14/Code.kt
+++ src/day14/Code.kt
@@ -33,21 +33,17 @@ fun main() {
         }
     }.count() - 1
 
-    fun part2(initial: Set<Coord>, max: Int) = generateSequence(initial) { current ->
-        generateSequence(Coord(500, 0)) { sand ->
-            listOf(
-                sand.copy(second = sand.second + 1),
-                sand.copy(first = sand.first - 1, second = sand.second + 1),
-                sand.copy(first = sand.first + 1, second = sand.second + 1)
-            ).firstOrNull {
-                it !in current && it.second < (max + 2)
-            }
-        }.last().takeIf {
-            it != Coord(500, 0)
-        }?.let {
-            current + it
-        }
-    }.count()
+    fun part2(initial: Set<Coord>, max: Int) =
+        generateSequence(Pair(initial, setOf(Coord(500, 0)))) { (current, newSand) ->
+            fun isRock(coord: Coord) = coord in initial || coord.second == (max + 2)
+            buildSet {
+                newSand.forEach { sand ->
+                    sand.copy(second = sand.second + 1).takeUnless(::isRock)?.let(::add)
+                    sand.copy(first = sand.first - 1, second = sand.second + 1).takeUnless(::isRock)?.let(::add)
+                    sand.copy(first = sand.first + 1, second = sand.second + 1).takeUnless(::isRock)?.let(::add)
+                }
+            }.takeIf { it.isNotEmpty() }?.let { (current + newSand) to it }
+        }.last().first.size - initial.size
 
     val (input, max) = parse(readInput(::main.javaClass.packageName))
     println("Part1=\n" + part1(input, max))
