9cb653bd6a5abb214a9310f7cac3d0a5a478a71a
Francesco Colasuonno <fcolasuonno@gmail.com>
2022-12-25 08:29:01 +0100

    day 25

--- src/day25/Code.kt
+++ src/day25/Code.kt
@@ -1,10 +1,38 @@
-package template
+package day25
 
 import day06.main
 import readInput
 
 fun main() {
-    fun part1(input: List<String>) = input.size
+    fun part1(input: List<String>) = input.sumOf {
+        it.fold(0L) { acc, c ->
+            acc * 5 + when (c) {
+                '2' -> 2
+                '1' -> 1
+                '0' -> 0
+                '-' -> -1
+                '=' -> -2
+                else -> throw UnsupportedOperationException()
+            }
+        }
+    }.let {
+        var num = it
+        buildString {
+            while (num > 0) {
+                append(
+                    when ((num + 2) % 5) {
+                        4L -> '2'
+                        3L -> '1'
+                        2L -> '0'
+                        1L -> '-'
+                        0L -> '='
+                        else -> throw UnsupportedOperationException()
+                    }
+                )
+                num = (num + 2) / 5L
+            }
+        }.reversed()
+    }
 
     val input = readInput(::main.javaClass.packageName)
     println("Part1=\n" + part1(input))
